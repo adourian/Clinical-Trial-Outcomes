93857516d5e445d24ca337041e7aac747f81bd85
adourian <127963164+adourian@users.noreply.github.com>
2024-09-13 10:39:28 +0200

    added docstrings

--- helpers.py
+++ helpers.py
@@ -10,6 +10,28 @@ from tqdm import tqdm
 
 
 def build_tinybioBERT_features(sentence_list):
+    """
+    Generate BERT embeddings for a list of sentences using the TinyBioBERT model.
+
+    This function tokenizes and processes each sentence in the provided list using the TinyBioBERT model. It computes the embeddings for the [CLS] token of each sentence and returns a list of embeddings. The function supports GPU acceleration if available.
+
+    Args:
+        sentence_list (list of str): A list of sentences for which embeddings will be generated.
+
+    Returns:
+        embeddings_list (list of numpy.ndarray): A list where each element is a numpy array containing the [CLS] token embeddings for a sentence. The embeddings are of shape (1, 768), where 768 is the dimension of the embeddings.
+
+    Raises:
+        ValueError: If `sentence_list` is not a list of strings.
+    
+    Example:
+        >>> sentences = ["This is a sample sentence.", "Another sentence for embedding."]
+        >>> features = build_tinybioBERT_features(sentences)
+        >>> len(features)
+        2
+        >>> features[0].shape
+        (1, 768)
+    """
 
     # Determine if a GPU is available and set the device accordingly
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # Check if GPU is available
@@ -49,6 +71,28 @@ def build_tinybioBERT_features(sentence_list):
     return embeddings_list
 
 def build_medBERT_features(sentence_list):
+    """
+    Generate BERT embeddings for a list of sentences using the MedBERT model.
+
+    This function tokenizes and processes each sentence in the provided list using the MedBERT model. It computes the embeddings for the [CLS] token of each sentence and returns a list of embeddings. The function supports GPU acceleration if available.
+
+    Args:
+        sentence_list (list of str): A list of sentences for which embeddings will be generated.
+
+    Returns:
+        embeddings_list (list of numpy.ndarray): A list where each element is a numpy array containing the [CLS] token embeddings for a sentence. The embeddings are of shape (1, 768), where 768 is the dimension of the embeddings.
+
+    Raises:
+        ValueError: If `sentence_list` is not a list of strings.
+
+    Example:
+        >>> sentences = ["This is a sample sentence.", "Another sentence for embedding."]
+        >>> features = build_medBERT_features(sentences)
+        >>> len(features)
+        2
+        >>> features[0].shape
+        (1, 768)
+    """
     # Determine if a GPU is available and set the device accordingly
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') 
     print(f'Using device: {device}')
@@ -95,6 +139,31 @@ def build_medBERT_features(sentence_list):
     return embeddings_list
 
 def build_BioSimCSE_features(sentence_list):
+    """
+    Generate embeddings for a list of sentences using the BioSimCSE model.
+
+    This function uses the BioSimCSE model from the SentenceTransformer library to generate
+    sentence embeddings. It determines if a GPU is available and uses it if possible. 
+    The embeddings are computed in evaluation mode and are converted to numpy arrays 
+    before being returned.
+
+    Parameters:
+    ----------
+    sentence_list : list of str
+        A list of sentences for which embeddings are to be generated.
+
+    Returns:
+    -------
+    embeddings_list : list of numpy.ndarray
+        A list where each element is a numpy array representing the embedding of the corresponding sentence.
+    
+    Notes:
+    -----
+    - The function uses the `kamalkraj/BioSimCSE-BioLinkBERT-BASE` model from the SentenceTransformer library.
+    - It prints the device being used (either CPU or GPU).
+    - The function handles GPU utilization if available, otherwise, it falls back to CPU.
+    """
+    
     # Determine if a GPU is available and set the device accordingly
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f'Using device: {device}')
@@ -126,6 +195,33 @@ def build_BioSimCSE_features(sentence_list):
     return embeddings_list
 
 def build_ChemBERTa_features(smiles_list):
+    """
+    Generate embeddings for a list of SMILES strings using the ChemBERTa model.
+
+    This function uses the ChemBERTa model from Hugging Face's `transformers` library 
+    to generate molecular embeddings from SMILES strings. It checks for GPU availability 
+    and utilizes it if possible. The embeddings are extracted from the [CLS] token of 
+    the model's output and are returned as numpy arrays.
+
+    Parameters:
+    ----------
+    smiles_list : list of str
+        A list of SMILES strings for which embeddings are to be generated.
+
+    Returns:
+    -------
+    embeddings_list : list of numpy.ndarray
+        A list where each element is a numpy array representing the embedding of the 
+        corresponding SMILES string.
+
+    Notes:
+    -----
+    - The function uses the `DeepChem/ChemBERTa-77M-MTR` model and tokenizer from the 
+      Hugging Face model hub.
+    - The model is set to evaluation mode to disable dropout and other training-specific 
+      layers.
+    - The function handles GPU utilization if available, otherwise, it defaults to CPU.
+    """
 
     # Determine if a GPU is available and set the device accordingly
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # Check if GPU is available
@@ -168,6 +264,35 @@ def build_ChemBERTa_features(smiles_list):
     return embeddings_list
 
 def longest_and_average_word_count(data, columns):
+    """
+    Calculate the longest and average word count for specified columns in a dataset.
+
+    This function processes each specified column in the input dataset to compute two metrics:
+    the maximum word count of any cell in the column and the average word count across all cells
+    in the column. It handles `None` values by ignoring them during calculations.
+
+    Parameters:
+    ----------
+    data : dict of list of str
+        A dictionary where keys are column names and values are lists of text entries (strings) 
+        for each column.
+
+    columns : list of str
+        A list of column names for which to calculate the longest and average word count.
+
+    Returns:
+    -------
+    results : dict
+        A dictionary where keys are column names and values are tuples containing two elements:
+        - The maximum word count in the column.
+        - The average word count in the column.
+
+    Notes:
+    -----
+    - If a column contains only `None` values, both the maximum and average word counts will be `0`.
+    - The function assumes that `data` contains lists of text for each column specified in `columns`.
+    """
+    
     results = {}
 
     for column in columns:
